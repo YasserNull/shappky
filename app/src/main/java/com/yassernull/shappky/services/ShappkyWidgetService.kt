afa7b249ea6861a735d2a2e6e35ba1106894a215
YasserNull <yasserbnull@gmail.com>
2026-07-26 01:37:14 +0100

    حل مشكلة تغيير حالة زر التطبيقات المحمية في ويدجيت القائمة

--- app/src/main/java/com/yassernull/shappky/services/ShappkyWidgetService.kt
+++ app/src/main/java/com/yassernull/shappky/services/ShappkyWidgetService.kt
@@ -58,7 +58,7 @@ class ShappkyWidgetFactory(
     val showUserApps = prefs.getBoolean(WidgetPreferences.getListShowUserAppsKey(appWidgetId), prefs.getBoolean(AppsListPreferences.KEY_SHOW_USER_APPS, true))
     val showSystemApps = prefs.getBoolean(WidgetPreferences.getListShowSystemAppsKey(appWidgetId), prefs.getBoolean(AppsListPreferences.KEY_SHOW_SYSTEM_APPS, true))
     val showPersistentApps = prefs.getBoolean(WidgetPreferences.getListShowPersistentAppsKey(appWidgetId), prefs.getBoolean(AppsListPreferences.KEY_SHOW_PERSISTENT_APPS, false))
-    val showProtectedApps = prefs.getBoolean(WidgetPreferences.getListShowProtectedAppsKey(appWidgetId), prefs.getBoolean(AppsListPreferences.KEY_SHOW_PROTECTED_APPS, false))
+    val showProtectedApps = prefs.getBoolean(WidgetPreferences.getListShowProtectedAppsKey(appWidgetId), false)
 
     manager.setShowUserApps(showUserApps)
     manager.setShowSystemApps(showSystemApps)
